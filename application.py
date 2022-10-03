52872ba99d99071589f520919d2d01004fab9429
Manohar <manohar@Manohars-MacBook-Pro.local>
2022-10-03 02:43:08 -0400

    update

--- application.py
+++ application.py
@@ -1,7 +1,18 @@
 from email.mime import application
+from flask import Flask, jsonify, request, render_template
 from flask import Flask
 application=Flask(__name__)
 
 @application.route('/')
 def helloWorld():
     return "hello world"
+@application.route('/test', methods=['GET', 'POST'])
+def testfn():
+    # GET request
+    if request.method == 'GET':
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers
+    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'Sucesss', 200
